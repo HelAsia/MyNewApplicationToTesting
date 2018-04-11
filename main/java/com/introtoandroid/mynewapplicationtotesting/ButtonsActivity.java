6185e406b986393994797bbb55359afdbe9d5d41
HelAsia <asiaheluszka1993@gmail.com>
2018-04-11 23:31:41 +0200

    MyChooseActivity and ButtonsActivity have been added

--- main/java/com/introtoandroid/mynewapplicationtotesting/ButtonsActivity.java
+++ main/java/com/introtoandroid/mynewapplicationtotesting/ButtonsActivity.java
@@ -25,95 +25,89 @@ public class ButtonsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buttons);
-    }
-    //Edit test with filter
 
-    final EditText text_filtred = (EditText) findViewById(R.id.editText);
-    text_filtred.setFilters(new InputFilter[]{
-        new InputFilter.AllCaps(),
-    });
+        //Edit test with filter
 
-    // Text with autocomplete
+        final EditText text_filtred = (EditText) findViewById(R.id.editText);
+        text_filtred.setFilters(new InputFilter[]{
+                new InputFilter.AllCaps()
+        });
 
-    final String[] COLORS = {
-            "czerwony", "zielony", "pomarańczowy", "purpurowy",
-            "czarny", "żółty", "szary", "fioletowy"
-    };
-    ArrayAdapter<String> adapter =
-            new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, COLORS);
-    AutoCompleteTextView text = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
-    text.setAdapter(adapter);
+        // Text with autocomplete
 
-    // Spinner
+        final String[] COLORS = {
+                "czerwony", "zielony", "pomarańczowy", "purpurowy",
+                "czarny", "żółty", "szary", "fioletowy"
+        };
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, COLORS);
+        AutoCompleteTextView text = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        text.setAdapter(adapter);
 
-    final Spinner spin = (Spinner) findViewById(R.id.spinner);
-    TextView textSel = (TextView) spin.getSelectedView();
+        // Spinner
 
-    // Button
+        final Spinner spin = (Spinner) findViewById(R.id.spinner);
+        TextView textSel = (TextView) spin.getSelectedView();
 
-    final Button basicButton = (Button) findViewById(R.id.button);
-    basicButton.setOnClickListener(new View.OnClickListener(){
-        public void onClick(View v){
-            context = getApplicationContext();
-            Intent intent = new Intent (context, DateActivity.class);
-            startActivity(intent);
-            Toast.makeText(ButtonsActivity.this,"Kliknięto przycisk",Toast.LENGTH_SHORT).show();
-        }
-    });
+        // Button
 
+        final Button basicButton = (Button) findViewById(R.id.button);
+        basicButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                context = getApplicationContext();
+                Intent intent = new Intent(context, DateActivity.class);
+                startActivity(intent);
+                Toast.makeText(ButtonsActivity.this, "Kliknięto przycisk", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        // ImageButton
 
-    // ImageButton
+        final ImageButton basicImageButton = (ImageButton) findViewById(R.id.imageButton);
+        basicImageButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(ButtonsActivity.this, "Kliknięto przycisk ze zdjęciem", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-    final ImageButton basicImageButton = (ImageButton) findViewById(R.id.imageButton);
-    basicImageButton.setOnClickListener(new View.OnClickListener()
+        // checkBox
 
-    {
-        public void onClick (View v){
-        Toast.makeText(ButtonsActivity.this, "Kliknięto przycisk ze zdjęciem", Toast.LENGTH_SHORT).show();
-    }
-    }
+        final CheckBox checkButton = (CheckBox) findViewById(R.id.checkBox);
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                CheckBox cb = (CheckBox) findViewById(R.id.checkBox);
+                cb.setText(checkButton.isChecked() ?
+                        "Pole zostało zaznaczone" :
+                        "Usunięto zaznaczenie pola");
+            }
+        });
 
-    );
+        //RadioGroup
 
-    // checkBox
+        final RadioGroup group = (RadioGroup) findViewById(R.id.RadioGroup01);
+        final TextView tv = (TextView) findViewById(R.id.textView2);
 
-    final CheckBox checkButton = (CheckBox) findViewById(R.id.checkBox);
-    checkButton.setOnClickListener(new View.OnClickListener(){
-        public void onClick (View v){
-            CheckBox cb = (CheckBox) findViewById(R.id.checkBox);
-            cb.setText(checkButton.isChecked() ?
-                    "Pole zostało zaznaczone":
-                    "Usunięto zaznaczenie pola");
-        }
-    });
-
-    //RadioGroup
-
-    final RadioGroup group = (RadioGroup) findViewById(R.id.RadioGroup01);
-    final TextView tv = (TextView)findViewById(R.id.textView2);
-
-    group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
-        public void onCheckedChanged(RadioGroup group, int checkedId){
-            if(checkedId !=-1) {
-                RadioButton rb = (RadioButton) findViewById(checkedId);
-                if (rb != null) {
-                    tv.setText("Wybrałeś: " + rb.getText());
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId != -1) {
+                    RadioButton rb = (RadioButton) findViewById(checkedId);
+                    if (rb != null) {
+                        tv.setText("Wybrałeś: " + rb.getText());
+                    }
+                } else {
+                    tv.setText("Wybierz jedną z opcji");
                 }
             }
-            else{
-                tv.setText("Wybierz jedną z opcji");
+        });
+
+        final Button clearChoice = (Button) findViewById(R.id.button2);
+        clearChoice.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                RadioGroup group = (RadioGroup) findViewById(R.id.RadioGroup01);
+                if (group != null) {
+                    group.clearCheck();
+                }
             }
-        }
-    });
-    final Button clearChoice = (Button) findViewById(R.id.button2);
-    clearChoice.setOnClickListener(new View.OnClickListener(){
-        public void onClick(View v){
-            RadioGroup group = (RadioGroup) findViewById(R.id.RadioGroup01);
-            if(group != null){
-                group.clearCheck();
-            }
-        }
-    });
-}
+        });
+    }
 }
--- main/java/com/introtoandroid/mynewapplicationtotesting/MyChooseActivity.java
+++ main/java/com/introtoandroid/mynewapplicationtotesting/MyChooseActivity.java
@@ -48,11 +48,10 @@ public class MyChooseActivity extends AppCompatActivity {
                 }else if (items[i] == element5) {
                     Intent intent = new Intent(contex, ExampleActivity.class);
                     startActivity(intent);
+                }else if (items[i] == element6) {
+                    Intent intent = new Intent(contex, ButtonsActivity.class);
+                    startActivity(intent);
                 }
-                //else if (items[i] == element6) {
-                  //  Intent intent = new Intent(contex, ButtonsActivity.class);
-                 //   startActivity(intent);
-               // }
             }
         });
     }
